f4f2f036a6168cebc5a69c61de7f3ccfcf0e5f61
Christos Hadjigeorghiou <87614879+ChristosHadjigeorghiou1996@users.noreply.github.com>
2023-05-29 20:24:12 +0200

    Update app.py
    cleaning

--- app.py
+++ app.py
@@ -131,8 +131,6 @@ def register():
     digest = md5(string_user_id_for_avatar.lower().encode('utf-8')).hexdigest()
     digest_image = "https://www.gravatar.com/avatar/"+digest+"?d=identicon&s="+'20'  
     
-    
-#    print(username)
     return render_template('index.html', digest_image = digest_image, user_id=user_id, user_name=user_name)
 
 
@@ -173,8 +171,6 @@ def login():
     digest = md5(string_user_id_for_avatar.lower().encode('utf-8')).hexdigest()
     digest_image = "https://www.gravatar.com/avatar/"+digest+"?d=identicon&s="+'20'  
       
-    
-#    print(username)
     return render_template('index.html', digest_image = digest_image, user_id=user_id, user_name=user_name)
 
 
@@ -186,21 +182,13 @@ def login():
 
 @app.route('/index', methods=['GET','POST'])
 def index():
-
-
-#    print(username)
     return render_template('login_2.html')
 
 
 
 @app.route('/logout')
 def logout():
-    return redirect(url_for('index'))
-#
-
-#   
-#
-    
+    return redirect(url_for('index'))    
     
 def edit_user(user_id,user_name):
     inputed_id = int(user_id)
@@ -256,77 +244,47 @@ def get_user_rated_books():
 
 
 def update_book_rating(user_id, book_title, new_rating):
-#    print('*'*10,' Update Book rating values','*'*10)
     inputed_id = int(user_id)
     cur = get_db().execute("SELECT book_data.ISBN FROM (`book_ratings` INNER JOIN `book_data` ON book_ratings.ISBN = book_data.ISBN) WHERE book_ratings.user_ID ={} AND book_data.book_title = '{}'".format(inputed_id, book_title))    
     data = cur.fetchone()
-#    print('data1: {}'.format(data))
     ISBN_chosen = data['ISBN']
-#    print('ISBN_chosen: {}'.format(ISBN_chosen))
-#    print('Id to search: {}'.format(inputed_id))
-#    print('Update to: {}'.format(new_rating))
     get_db().execute("UPDATE book_ratings SET book_rating = {} WHERE user_ID = {} and ISBN = '{}'".format(new_rating, inputed_id, ISBN_chosen))
     get_db().commit()
     cur = get_db().execute("SELECT * from book_ratings where user_ID = {} and ISBN = '{}'".format(inputed_id,ISBN_chosen))
     data = cur.fetchone()
-#    print('de dame: {}'.format(data))
     return data
 
 @app.route('/update_rating', methods=['GET', 'POST'])
 def update_rating():
     print('received from html: {}'.format(request.form))
     user_id = request.form.get('user_id')
-#    print("USER ID: " , user_id)
     int_user_id = int(user_id)
     book_title = request.form.get('book_title')
-#    print('BOOOOK TITLE  : ' , book_title)
     rating = request.form.get('rating')
-#    print('RATINGGGG : ' , rating)
     data = update_book_rating(int_user_id, book_title, rating)
-#    print('data received from inner function: {}'.format(data))
     book_rating_verification = data['book_rating']
-#    print('book_rating_verification: {}'.format(book_rating_verification))
     ISBN_verification = data['ISBN']
-#    print('ISBN_verification: {}'.format(ISBN_verification))
     
     return 'Successful Update of rating'  
 
 def delete_book_rating(user_id, book_title):
-#    print('*'*10,' Delete Book rating values','*'*10)
     inputed_id = int(user_id)
     cur = get_db().execute("SELECT book_data.ISBN FROM (`book_ratings` INNER JOIN `book_data` ON book_ratings.ISBN = book_data.ISBN) WHERE book_ratings.user_ID ={} AND book_data.book_title = '{}'".format(inputed_id, book_title))    
     data = cur.fetchone()
-#    print('data1: {}'.format(data))
     ISBN_chosen = data['ISBN']
-#    print('ISBN_chosen: {}'.format(ISBN_chosen))
-#    print('Id to search: {}'.format(inputed_id))
     get_db().execute("DELETE FROM book_ratings WHERE user_ID = {} and ISBN = '{}'".format(inputed_id, ISBN_chosen))
     get_db().commit()
     cur = get_db().execute("SELECT * from book_ratings where user_ID = {} ".format(inputed_id))
     data = cur.fetchone()
-#    print('de dame: {}'.format(data))
 
 @app.route('/delete_rating', methods=['GET', 'POST'])
 def delete_rating():
-#    print('received from html: {}'.format(request.form))
     user_id = request.form.get('user_id')
-#    print("USER ID: " , user_id)
     int_user_id = int(user_id)
     book_title = request.form.get('book_title')
-#    print('BOOOOK TITLE  : ' , book_title)
     delete_book_rating(int_user_id, book_title)
     
     return 'Successful Delete of rating'  
-
-
-
-
-
-
-
-
-
-
 
 def user_unrated_books(user_id):
     cur = get_db().execute("SELECT DISTINCT book_data.book_title FROM `book_data` WHERE book_data.book_title NOT IN ( SELECT book_data.book_title FROM (`book_ratings` INNER JOIN `book_data` ON book_ratings.ISBN = book_data.ISBN) WHERE user_ID = {})".format(user_id))
@@ -339,50 +297,33 @@ def get_unrated_books():
     int_user_id = int(user_id)
     unrated_books_received = user_unrated_books(int_user_id)
     unrated_books_sent = jsonify(unrated_books_received)
-#    print('unrated_books_sent:{}',unrated_books_sent)
 
     return unrated_books_sent
 
 #emina dame 
 
 def add_book_rating(user_id, book_title, new_rating):
-#    print('*'*10,' Add Book rating values','*'*10)
     inputed_id = int(user_id)
-#    print('user_id: ',user_id)
-#    print('BOOOOK TITLE 2 : ' , book_title)
-#    print('RATINGGGG  2 : ' , new_rating)
     
     cur = get_db().execute("SELECT book_data.ISBN FROM `book_data` WHERE book_data.book_title ='{}'".format(book_title))    
     data = cur.fetchone()
-#    print('data1: {}'.format(data))
     ISBN_chosen = data['ISBN']
-#    print('ISBN_chosen: {}'.format(ISBN_chosen))
-#    print('Id to add: {}'.format(inputed_id))
-#    print('Value to add : {}'.format(new_rating))
     get_db().execute("INSERT INTO book_ratings VALUES ({}, '{}', {})".format(inputed_id, ISBN_chosen, new_rating))
     get_db().commit()
     
     cur = get_db().execute("SELECT * from book_ratings where user_ID = {} and ISBN = '{}'".format(inputed_id,ISBN_chosen))
     data = cur.fetchone()
-#    print('de dame: {}'.format(data))
     return data
 
 @app.route('/add_new_rating', methods=['GET', 'POST'])
 def add_new_rating():
-#    print('received from html: {}'.format(request.form))
     user_id = request.form.get('user_id')
-#    print("USER ID: " , user_id)
     int_user_id = int(user_id)
     book_title = request.form.get('book_title')
-#    print('BOOOOK TITLE 2 : ' , book_title)
     rating = request.form.get('rating')
-#    print('RATINGGGG : ' , rating)
     data = add_book_rating(int_user_id, book_title, rating)
-#    print('data received from inner function: {}'.format(data))
     book_rating_verification = data['book_rating']
-#    print('book_rating_verification: {}'.format(book_rating_verification))
     ISBN_verification = data['ISBN']
-#    print('ISBN_verification: {}'.format(ISBN_verification))
     
     return 'Successful Addition of rating' 
 
@@ -400,10 +341,7 @@ def helper_function(predictions_df, userID, movies_df, original_ratings_df, num_
     user_full = (user_data.merge(movies_df, how = 'left', left_on = 'ISBN', right_on = 'ISBN').
                      sort_values(['book_rating'], ascending=False)
                  )
-
-#    print ('User {0} has already rated {1} movies.'.format(userID, user_full.shape[0]))
-#    print ('Recommending the highest {0} predicted ratings movies not already rated.'.format(num_recommendations))
-    
+   
     # Recommend the highest predicted rating movies that the user hasn't seen yet.
     recommendations = (movies_df[~movies_df['ISBN'].isin(user_full['ISBN'])].
          merge(pd.DataFrame(sorted_user_predictions).reset_index(), how = 'left',
@@ -418,23 +356,15 @@ def helper_function(predictions_df, userID, movies_df, original_ratings_df, num_
 
 # get the two tables
 def recommend_books(num_recommendations = 2):
-#    print('received from html: {}'.format(request.form))
     user_id = request.form.get('user_id')
-#    print("USER ID: " , user_id)
     user_id = int(user_id)
     
-    # user_id = request.form.get('user_id')
-#    print("USER ID: " , user_id)
-    # combine tables on ISBN
-#    cur = get_db().execute("SELECT * FROM (`book_ratings` INNER JOIN `book_data` ON book_ratings.ISBN = book_data.ISBN)")
     cur = get_db().execute("SELECT * FROM book_data")
     data_books = cur.fetchall()
     books_df = pd.DataFrame(data_books)
-#    print('books_df', books_df)
     cur = get_db().execute("SELECT * FROM book_ratings")
     data_ratings = cur.fetchall()
     book_rating_df = pd.DataFrame(data_ratings)
-#    print('book_rating_df:',book_rating_df)
 
     # perform SVD
 
@@ -450,11 +380,7 @@ def recommend_books(num_recommendations = 2):
     all_user_predicted_ratings_df = np.dot(np.dot(U, sigma), Vt) + user_ratings_mean.reshape(-1, 1)
     preds_df = pd.DataFrame(all_user_predicted_ratings_df, columns= R_df.columns)
     
-#    print('preds_df:\n')
-#    print(preds_df.head())  
     already_rated, predictions = helper_function(preds_df, user_id, books_df, book_rating_df, num_recommendations)
-#    print('ALREADY RATED: ' ,already_rated.values.tolist())
-#    print('RECOMMEND: ' , predictions.values.tolist())
     list_to_return_already_rated = []
     for i in already_rated.values.tolist():
         print(i)
@@ -477,11 +403,7 @@ def recommend_books(num_recommendations = 2):
         dict2['book_title'] = j[3]
         list_to_return_predictions.append(dict2)
     
-        
-#    print('list_to_return_already_rated: ' , list_to_return_already_rated)
-#    print('list_to_return_predictions:', list_to_return_predictions)
     return list_to_return_already_rated, list_to_return_predictions
-    # get user ratings in descending order
     
     
  # recommend only top 2 books if available else, whatever is available in chosen category
@@ -492,15 +414,7 @@ def get_rec():
 @app.route('/get_genre_rec', methods=['GET','POST'])
 def get_gentre_rec():
     return jsonify(recommend_books())
-    
-    
-    
-    
-    
-# already_rated, predictions= recommend_books(preds_df, 2, book_data_df, user_book_rating_df, num_recommendations = 3)
-
-    
-    
+ 
 
 if  __name__ == "__main__" :
 	app.run()
